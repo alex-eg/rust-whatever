83626ae347d18237ad75f162b6bdf0b92895dab8
taptap <alexey.e.egorov@gmail.com>
2014-04-28 02:08:13 +0400

    snafu

--- string-palindroms.rs
+++ string-palindroms.rs
@@ -1,7 +1,26 @@
 use std::io;
 
-fn main()
-{
-    let line = io::stdin().lines()[0];
-    println!(" {:?}", line);
+fn reverse(s: ~str) -> ~str {
+    s.chars_rev().collect()
+}
+
+fn get_palindroms(s: ~str) -> ~[~str] {
+    let words:~[~str] = s.split_terminator(' ').collect();
+    let p : ~[~str] = words.iter().filter(|x| reverse(*x) == x).collect();
+    p
+}   
+
+fn main() {
+    for line in io::stdin().lines() {
+        let s = line.unwrap();
+        let p_list = get_palindroms(s);
+        if p_list.len() == 0 {
+            println!("No palindroms found!");
+        } else {
+            println!("Found palindroms:");
+            for p in p_list.move_iter() {
+                println!("{}", p);
+            }
+        }
+    }
 }
