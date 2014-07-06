dd5010ac41e4dae80e7e651fc66e7dcb4569ea94
taptap <alexey.e.egorov@gmail.com>
2014-07-06 23:48:29 +0400

    fix

--- lucky.rs
+++ lucky.rs
@@ -1,14 +1,9 @@
+use std::io;
+
 fn main()
 {
-    let mut lines = [];
-    let reader = io::sidin();
-    println!("Input number: ");
-    while !reader.eof() {
-        allLines.push((reader as io:ReaderUtil).read_line());
-    }
-    
-    for allLines.each |line| {
-        println!("{}", line);
+    for line in io::stdin().lines() {
+        println!("{}", line.unwrap());
     }
 }
     
