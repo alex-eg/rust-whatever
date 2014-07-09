b635946e2b9bf642e506cde8d4aea848bba1f25b
taptap <alexey.e.egorov@gmail.com>
2014-07-09 16:27:01 +0400

    add type polymorphism to bst container

--- bst.rs
+++ bst.rs
@@ -1,10 +1,12 @@
-enum Bst {
-    Node(i32, Box<Bst>, Box<Bst>),
+use std::fmt::Show;
+
+enum Bst<T> {
+    Node(T, Box<Bst<T>>, Box<Bst<T>>),
     Nil
 }
 
-impl Bst {
-    fn add(& mut self, v: i32) {
+impl <T:PartialOrd + Show> Bst<T> {
+    fn add<T>(&mut self, v: T) -> () {
         match self {
             &Nil => *self = Node(v, box Nil, box Nil),
             &Node(ref x, ref mut x_left, ref mut x_right) => {
@@ -18,9 +20,9 @@ impl Bst {
         match self {
             &Nil => print!("nil"),
             &Node(ref x, ref x_left, ref x_right) =>
-                { print!("Bt({},", x);
+                { print!("Bt({}, ", x);
                   x_left.print();
-                  print!(",");
+                  print!(", ");
                   x_right.print();
                   print!(")");
                 }
@@ -30,10 +32,11 @@ impl Bst {
 
 fn main()
 {
-    let mut a = Nil;
-    a.add(3);
+    let mut a: Bst<int> = Nil;
+    a.add(3i);
     a.add(1);
     a.add(2);
     a.add(4);
     a.print();
+    println!("");
 }
