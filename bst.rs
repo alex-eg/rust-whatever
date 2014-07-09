13e39fbe3b4d6eca8e83e152eadba5c39d5968d4
taptap <alexey.e.egorov@gmail.com>
2014-07-09 16:45:25 +0400

    cosmetic changes

--- bst.rs
+++ bst.rs
@@ -1,17 +1,18 @@
 use std::fmt::Show;
+use std::option::Option;
 
 enum Bst<T> {
     Node(T, Box<Bst<T>>, Box<Bst<T>>),
     Nil
 }
 
-impl <T:PartialOrd + Show> Bst<T> {
+impl <T:Ord + Show> Bst<T> {
     fn add<T>(&mut self, v: T) -> () {
         match self {
             &Nil => *self = Node(v, box Nil, box Nil),
-            &Node(ref x, ref mut x_left, ref mut x_right) => {
-                if v > *x { x_right.add(v) }
-                else if v < *x { x_left.add(v) }
+            &Node(x, ref mut left, ref mut right) => {
+                if v > x { right.add(v) }
+                else if v < x { left.add(v) }
             }
         }
     }
@@ -19,11 +20,11 @@ impl <T:PartialOrd + Show> Bst<T> {
     fn print(&self) {
         match self {
             &Nil => print!("nil"),
-            &Node(ref x, ref x_left, ref x_right) =>
+            &Node(x, ref left, ref right) =>
                 { print!("Bt({}, ", x);
-                  x_left.print();
+                  left.print();
                   print!(", ");
-                  x_right.print();
+                  right.print();
                   print!(")");
                 }
         }
